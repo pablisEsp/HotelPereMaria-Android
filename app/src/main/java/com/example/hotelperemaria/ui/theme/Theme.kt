794ff4f16f0ab1d8050ff7f0198228c04b75d14b
FranMor97 <fran.moreno.1234@gmail.com>
2025-01-09 16:30:52 +0100

    Added navigation

--- app/src/main/java/com/example/hotelperemaria/ui/theme/Theme.kt
+++ app/src/main/java/com/example/hotelperemaria/ui/theme/Theme.kt
@@ -1,14 +1,10 @@
 package com.example.hotelperemaria.ui.theme
 
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.ShapeDefaults
-import androidx.compose.material3.Shapes
-import androidx.compose.material3.Typography
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
-
 
 
 private val DarkColorScheme = darkColorScheme(
