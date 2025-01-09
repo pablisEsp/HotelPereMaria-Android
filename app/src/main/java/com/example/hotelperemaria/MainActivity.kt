a5ae46b621bb2843aef7edfb8e10032a48e26fb5
pablisEsp <pabelslopez@gmail.com>
2025-01-09 17:19:17 +0100

    algo

--- app/src/main/java/com/example/hotelperemaria/MainActivity.kt
+++ app/src/main/java/com/example/hotelperemaria/MainActivity.kt
@@ -21,6 +21,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             HotelPereMariaTheme {
                 AppNavigation()
+                //cksagzahj
             }
         }
     }
