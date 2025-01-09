d3cd28203d9f0ca5d4294de1cc715fe296ca470b
FranMor97 <fran.moreno.1234@gmail.com>
2025-01-09 16:38:40 +0100

    prueba

--- app/src/main/java/com/example/hotelperemaria/navigation/AppScreens.kt
+++ app/src/main/java/com/example/hotelperemaria/navigation/AppScreens.kt
@@ -4,4 +4,4 @@ sealed class AppScreens(val route: String) {
     data object FirstScreen: AppScreens("first_screen")
     data object SecondScreen: AppScreens("second_screen")
 }
-//this are simulated screens , here should be the reals ones, those are examples for you to replace with yours screens
+//this are simulated screens , here should be the reals ones, those are examples for you to replace with  yours screens
