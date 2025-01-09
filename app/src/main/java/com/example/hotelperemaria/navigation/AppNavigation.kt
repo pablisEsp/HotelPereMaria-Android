34f6bbdf1fa4b399cb7b7c1db64bad37271d2b3b
pablisEsp <pabelslopez@gmail.com>
2025-01-09 17:30:43 +0100

    starting project

--- app/src/main/java/com/example/hotelperemaria/navigation/AppNavigation.kt
+++ app/src/main/java/com/example/hotelperemaria/navigation/AppNavigation.kt
@@ -4,17 +4,15 @@ import androidx.compose.ui.text.Placeholder
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.hotelperemaria.home.HomeScreen
 import com.example.hotelperemaria.navigation.AppScreens
 
 @Composable
 fun AppNavigation() {
     val navController = rememberNavController()
-    NavHost(navController = navController, startDestination = AppScreens.FirstScreen.route ){
-        composable(route = AppScreens.FirstScreen.route) {
-            Text("Aqui va la primera pantalla")
-        }
-        composable(route = AppScreens.SecondScreen.route) {
-            Text("Aqui va la segunda pantalla")
+    NavHost(navController = navController, startDestination = AppScreens.homeScreen.route ){
+        composable(route = AppScreens.homeScreen.route) {
+            HomeScreen(navController)
         }
 
     }
