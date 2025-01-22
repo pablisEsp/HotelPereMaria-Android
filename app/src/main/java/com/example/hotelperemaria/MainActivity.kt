92de392420ac505c392bf59f6e759302b61f40c4
FranMor97 <fran.moreno.1234@gmail.com>
2025-01-22 19:48:19 +0100

    Things work

--- app/src/main/java/com/example/hotelperemaria/MainActivity.kt
+++ app/src/main/java/com/example/hotelperemaria/MainActivity.kt
@@ -1,11 +1,12 @@
    package com.example.hotelperemaria
 
 
-import AppNavigation
+
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import com.example.hotelperemaria.navigation.AppNavigation
 import com.example.hotelperemaria.ui.theme.HotelPereMariaTheme
 
    class MainActivity : ComponentActivity() {
