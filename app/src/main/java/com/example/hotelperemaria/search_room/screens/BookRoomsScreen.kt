9c9081f2a2c0e4d8d8d187e2bc17a618376c8dae
FranMor97 <fran.moreno.1234@gmail.com>
2025-01-27 20:25:19 +0100

    Added some thing

--- app/src/main/java/com/example/hotelperemaria/search_room/screens/BookRoomsScreen.kt
+++ app/src/main/java/com/example/hotelperemaria/search_room/screens/BookRoomsScreen.kt
@@ -1,26 +1,11 @@
 package com.example.hotelperemaria.search_room.screens
 
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
-import com.example.hotelperemaria.R
-import com.example.hotelperemaria.search_room.screens.date_picker.DatePickerCustom
+import com.example.hotelperemaria.search_room.screens.Widgets.DatePickerCustom
 import com.example.hotelperemaria.utils.BottomAppBarCustom
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -43,4 +28,6 @@ fun BookRoomsScreen(navController: NavController) {
 
     }
 
+
+    DatePicker()
 }
---
+++ app/src/main/java/com/example/hotelperemaria/search_room/screens/Widgets/date_picker.kt
@@ -0,0 +1,208 @@
+package com.example.hotelperemaria.search_room.screens.widgets
+
+import android.icu.text.SimpleDateFormat
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.text.selection.TextSelectionColors
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.CalendarMonth
+import androidx.compose.material3.DatePicker
+import androidx.compose.material3.DatePickerDefaults
+import androidx.compose.material3.DatePickerDialog
+import androidx.compose.material3.DatePickerState
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextFieldColors
+import androidx.compose.material3.rememberDatePickerState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.hotelperemaria.ui.theme.HotelPereMariaTheme
+import com.example.hotelperemaria.ui.theme.darkGray
+import com.example.hotelperemaria.ui.theme.lightGray
+import com.example.hotelperemaria.ui.theme.white
+import com.example.hotelperemaria.ui.theme.whitesmoke
+import java.util.Date
+import java.util.Locale
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DatePickerDialogCustom(modifier: Modifier) {
+
+    val datePickerState = rememberDatePickerState()
+    var showDatePicker by remember {
+        mutableStateOf(false)
+    }
+    var selectedDate by remember {
+        mutableStateOf("")
+    }
+//in the future change the modifier
+    val gradientBrush = Brush.linearGradient(
+        colors = listOf(
+            Color(0xFF4D97A2), // Azul oscuro
+            Color(0xFF78C6D2)  // Azul más claro
+        )
+    )
+
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(12.dp),
+        horizontalArrangement = Arrangement.Start
+    ) {
+        OutlinedTextField(
+            colors = TextFieldColors(
+                disabledContainerColor = darkGray.copy(alpha = 0.8f),
+                focusedTextColor = white,
+                unfocusedTextColor = white,
+                disabledTextColor = white,
+                errorTextColor = Color.Red, // Magenta
+                focusedContainerColor =  lightGray.copy(alpha = 0.8f), // Naranja coral oscuro
+                unfocusedContainerColor = darkGray.copy(alpha = 0.8f), // Azul oscuro
+                errorContainerColor = Color.Magenta,
+                cursorColor = white,
+                errorCursorColor = Color.Red,
+                textSelectionColors = TextSelectionColors(
+                    handleColor = white,
+                    backgroundColor = white, // Fondo selección naranja coral
+                ),
+                focusedIndicatorColor = white,
+                unfocusedIndicatorColor = white,
+                disabledIndicatorColor = white,
+                errorIndicatorColor = Color.Magenta,
+                focusedLeadingIconColor = white, // Blanco para consistencia con texto
+                unfocusedLeadingIconColor = white, // Gris claro
+                disabledLeadingIconColor = white, // Gris claro
+                errorLeadingIconColor = Color.Magenta, // Magenta para errores
+                focusedTrailingIconColor = white,
+                unfocusedTrailingIconColor = white,
+                disabledTrailingIconColor = white,
+                errorTrailingIconColor = Color.Magenta,
+                focusedLabelColor = Color.Black, // Naranja coral
+                unfocusedLabelColor = white,
+                disabledLabelColor = white,
+                errorLabelColor = Color.Magenta,
+                focusedPlaceholderColor = whitesmoke, // Naranja coral
+                unfocusedPlaceholderColor = whitesmoke,
+                disabledPlaceholderColor = whitesmoke,
+                errorPlaceholderColor = Color.Magenta,
+                focusedSupportingTextColor = white, // Texto blanco en estado enfocado
+                unfocusedSupportingTextColor = whitesmoke,
+                disabledSupportingTextColor = whitesmoke,
+                errorSupportingTextColor = Color.Magenta,
+                focusedPrefixColor = lightGray.copy(0.8f), // Naranja coral
+                unfocusedPrefixColor = whitesmoke,
+                disabledPrefixColor = whitesmoke,
+                errorPrefixColor = Color.Magenta,
+                focusedSuffixColor = lightGray, // Naranja coral
+                unfocusedSuffixColor = darkGray,
+                disabledSuffixColor = darkGray,
+                errorSuffixColor = Color.Magenta
+            ),
+            shape = CircleShape,
+            value = selectedDate,
+            enabled = false,
+            onValueChange = {},
+            label = { Text(text = "Selecione Una Fecha") },
+            readOnly = true,
+            trailingIcon = {
+                IconButton(onClick = { showDatePicker = true }) {
+                    Icon(
+                        imageVector = Icons.Default.CalendarMonth,
+                        contentDescription = "Select date"
+                    )
+                }
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(64.dp)
+        )
+        if (showDatePicker) {
+            DatePickerModal(
+                datePickerState = datePickerState,
+                onDateSelected = { it?.let { selectedDate = convertMillisToDate(it) } }) {
+                showDatePicker = false
+            }
+        }
+    }
+}
+
+fun convertMillisToDate(millis: Long): String {
+    val formatter = SimpleDateFormat("MM/dd/yyyy", Locale.getDefault())
+    return formatter.format(Date(millis))
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DatePickerModal(
+    datePickerState: DatePickerState,
+    onDateSelected: (Long?) -> Unit,
+    onDismiss: () -> Unit,
+
+    ) {
+
+    DatePickerDialog(
+
+        onDismissRequest = onDismiss,
+        confirmButton = {
+            TextButton(onClick = {
+                onDateSelected(datePickerState.selectedDateMillis)
+                onDismiss()
+            }) {
+                Text("OK", color = Color(0xFF1d393d))
+            }
+        },
+        dismissButton = {
+            TextButton(onClick = onDismiss) {
+                Text("Cancel", color = Color(0xFF1d393d))
+            }
+        },
+        colors = DatePickerDefaults.colors(
+            containerColor = lightGray, // Fondo azul oscuro
+            titleContentColor = white, // Título blanco
+            headlineContentColor = white, // Texto principal
+            weekdayContentColor = darkGray, // Días de la semana
+            dayContentColor = whitesmoke, // Días normales
+            selectedDayContentColor = white, // Día seleccionado
+            selectedDayContainerColor = darkGray.copy(alpha = 0.8f), // Fondo del día seleccionado
+            todayContentColor = Color.Black, // Hoy resaltado
+            disabledDayContentColor = whitesmoke.copy(alpha = 0.3f), // Días deshabilitados
+        )
+    ) {
+        DatePicker(
+            state = datePickerState,
+            colors = DatePickerDefaults.colors(
+                selectedDayContainerColor = darkGray.copy(alpha = 0.3f), // Fondo del día seleccionado
+                selectedDayContentColor = whitesmoke, // Texto del día seleccionado
+                todayDateBorderColor = whitesmoke,
+                todayContentColor = whitesmoke// Borde rojo para el día de hoy
+            ),
+
+            )
+    }
+}
+
+
+@Composable
+@Preview
+fun DatePickerDialogCustomPreview() {
+    HotelPereMariaTheme {
+        DatePickerDialogCustom(modifier = Modifier)
+    }
+}
