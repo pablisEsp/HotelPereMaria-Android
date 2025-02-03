d4e8a4375c717307a64899b584cdaf0d95b89ad8
pablisEsp <pabelslopez@gmail.com>
2025-02-03 01:11:48 +0100

    commiting

--- app/src/main/java/com/example/hotelperemaria/home/homeScreen.kt
+++ app/src/main/java/com/example/hotelperemaria/home/homeScreen.kt
@@ -1,16 +1,7 @@
 package com.example.hotelperemaria.home
 
-import PantallaInicio
 import PantallaInicioConTutorial
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material.icons.Icons
-import androidx.compose
-    .material.icons.filled.*
-import androidx.compose.material.icons.filled.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -18,17 +9,9 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
-import com.example.hotelperemaria.R
-import com.example.hotelperemaria.rooms.model.Habitacion
 import com.example.hotelperemaria.rooms.viewmodel.RoomViewModel
-import com.example.hotelperemaria.utils.BottomAppBarCustom
-
-
 
 
 @Composable
@@ -53,3 +36,4 @@ fun HomeScreen(navController: NavController, viewModel: RoomViewModel = viewMode
 }
 
 
+//a
